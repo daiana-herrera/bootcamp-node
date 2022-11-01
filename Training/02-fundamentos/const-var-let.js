b59628a0e1dc38088593eb3896fe549f2941f176
daiana-herrera <daiana.herrera@accenture.com>
2022-11-01 16:42:45 -0300

    seccion 3 terminada

---
+++ Training/02-fundamentos/const-var-let.js
@@ -0,0 +1,11 @@
+const nombre ='wolverine';
+
+if (true){
+   const nombre = 'Magneto';
+}
+
+console.log(nombre);
+
+//no usar nunca var
+//usar let o const si no la pienso cambiar.
+
---
+++ Training/02-fundamentos/template-string.js
@@ -0,0 +1,17 @@
+const nombre = 'Deadpool';
+const real='Wade Winston';
+
+const normal = nombre +' '+ real;
+const template = `${nombre} ${real}`;
+
+
+console.log(normal);
+console.log(template);
+console.log(normal===template);
+
+const html=`
+<H1>Hola mundo!</H1>
+
+`;
+
+console.log(html);
